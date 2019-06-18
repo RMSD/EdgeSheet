ef4c22bff7b63a7485af9d5a9984942f471ecd35
Rajat Murthi <Rajat.Murthi@accolade.com>
2019-06-18 16:38:41 -0700

    Update site metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/EdgeSheet",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `FFG Star Wars Character Sheet Builder`,
+    description: `Build character sheets for all the FFG Star Wars RPGs.`,
+    author: `@Living_Target`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
