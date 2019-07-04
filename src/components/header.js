575bc89334acc21f264b8f547aa9a91f03f647ff
Rajat Murthi <rangers1500@yahoo.com>
2019-07-03 23:31:06 -0700

    Quick Alpha header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,3 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
@@ -17,15 +16,7 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
           {siteTitle}
-        </Link>
       </h1>
     </div>
   </header>
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,8 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
-// import Header from "./header"
+import Header from "./header"
 import "./css/skeleton.css"
 import "./css/normalize.css"
 import NameplateStateWrapper from "./contexts/nameplate-context";
@@ -17,36 +16,22 @@ import CharacteristicStateWrapper from "./contexts/characteristic-context";
 import SkillStateWrapper from "./contexts/skills-context";
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <NameplateStateWrapper>
-          <CharacteristicStateWrapper>
-            <SkillStateWrapper>
-              <main>{children}</main>
-            </SkillStateWrapper>
-          </CharacteristicStateWrapper>
-        </NameplateStateWrapper>
-        <br />
-        <div className="container">
-          <footer>
-            © {new Date().getFullYear()}, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-
-      </>
-    )}
-  />
+  <>
+    <Header siteTitle="ALPHA VERSION" />
+    <NameplateStateWrapper>
+      <CharacteristicStateWrapper>
+        <SkillStateWrapper>
+          <main>{children}</main>
+        </SkillStateWrapper>
+      </CharacteristicStateWrapper>
+    </NameplateStateWrapper>
+    <br />
+    <div className="container">
+      <footer>
+        © {new Date().getFullYear()}, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  </>
 )
 
 Layout.propTypes = {
