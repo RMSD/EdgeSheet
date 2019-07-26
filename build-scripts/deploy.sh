167db309577044fb38dc066615b44672fb1368b6
Rajat Murthi <Rajat.Murthi@accolade.com>
2019-07-26 03:05:55 -0700

    Test out some config changes.

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -2,5 +2,5 @@ git init
 git config user.name "Travis-CI"
 git config user.email "travis@CI.com"
 npm run deploy
-git add -A
+git add .
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/RMSD/EdgeSheet.git" master:gh-pages > /dev/null 2>&1
