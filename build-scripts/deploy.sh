32e3a00165a9c2599564d52030cc58b83d522d93
Rajat Murthi <Rajat.Murthi@accolade.com>
2019-09-30 11:47:36 -0700

    Remove deploy script.

--- build-scripts/deploy.sh
+++
@@ -1,6 +0,0 @@
-git init
-git config user.name "Travis-CI"
-git config user.email "travis@CI.com"
-npm run deploy
-git add .
-git push --force --quiet "https://${GITHUB_TOKEN}@github.com/RMSD/EdgeSheet.git" master:gh-pages > /dev/null 2>&1
