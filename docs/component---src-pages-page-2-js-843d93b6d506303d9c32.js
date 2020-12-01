07ea52506d809bc762800d2fc6be794a96116ff8
RMSD <RMSD@users.noreply.github.com>
2020-12-01 03:18:49 -0800

    Clean start.

--- docs/component---src-pages-page-2-js-843d93b6d506303d9c32.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{216:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(148),c=a(214),r=a(213);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-843d93b6d506303d9c32.js.map
